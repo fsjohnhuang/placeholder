6e392edcc8e85cae94615d852aaedf92548f2d07
fsjohnhuang <fsjohnhuang@hotmail.com>
2014-08-25 23:42:44 +0800

    完成placeholder UI生成部分

--- polyfill_placeholder.js
+++ polyfill_placeholder.js
@@ -3,108 +3,146 @@
  * @version v0.1
  */
 ;(function(exports){
-	// 判断是否使用W3C的CSS盒子模型
+	var _factory = document.createElement('DIV');
+	var _$ = function (html){
+			_factory.innerHTML = html;	
+			return _factory.firstChild;
+		};
+	// 判断是否为W3C标准的盒子模式
 	var _isBoxModel = (function(){
-		var tmp = document.createElement('div');
-		tmp.style.cssText = 'width:1px; position: absolute;';
+		var tmp = _$('<div style="padding-left:1px;width:1px; position: absolute;"></div>');
 		document.body.appendChild(tmp);
-		tmp.style.paddingLeft = '1px';
 		var ret = tmp.offsetWidth === 1;
 		document.body.removeChild(tmp);
 
 		return ret;
 	}());
-
-	var defaultBorderWidth = 1,
-		defaultPaddingWidth = 2;
-	var _getW = function(el){
-		var w = el.offsetWidth;
-		if (_isBoxModel) return w;
-
-		var bl = parseInt(_getComputedStyle(el, 'borderLeft')) || defaultBorderWidth,
-			br = parseInt(_getComputedStyle(el, 'borderRight')) || defaultBorderWidth;
-		var pl = parseInt(_getComputedStyle(el, 'paddingLeft')) || defaultPaddingWidth,
-			pr = parseInt(_getComputedStyle(el, 'paddingRight')) || defaultPaddingWidth;
-		w = w - bl - br - pl - pr;
-		return w;
-	};
-	var _getH = function(el){
-		var h = el.offsetHeight;
-		if (_isBoxModel) return h;
-
-		var bt = parseInt(_getComputedStyle(el, 'borderTop')) || defaultBorderWidth,
-			bb = parseInt(_getComputedStyle(el, 'borderBottom')) || defaultBorderWidth;
-		var pt = parseInt(_getComputedStyle(el, 'paddingTop')) || defaultPaddingWidth,
-			pb = parseInt(_getComputedStyle(el, 'paddingBottom')) || defaultPaddingWidth;
-		h = h - bt - bb - pt - pb;
-		return h;
-	};
-
+	// 获取计算后的样式值
 	var _getComputedStyle = function(el, prop){
-		return el.currentStyle[prop];
-	}
-	var _getInputTopLeft = function(el){
-		var pos = _getComputedStyle(el, 'position');
-		if (pos !== 'relative' || pos !== 'absolute'){
-			el.style.position = 'relative';
+		var kv ;
+		if (el.currentStyle){
+			kv =  el.currentStyle;
+		}else{
+			kv = window.getComputedStyle(el, null);
 		}
 
-		return {
-			top: el.offsetTop,
-			left: el.offsetLeft
+		return kv[prop];
+	}
+	// 文本模板
+	var _fmt = function(str/*arg1,arg2...*/){
+		var args = [].slice.call(arguments, 1);
+		return str.replace(/\$\{[^}{]+\}/g, function(orig){
+				var k = /\$\{([^}{]+)\}/.exec(orig)[1];
+				return args[k] || args[0][k] || '';
+			});
+	};
+
+	var _getStyle = function(el, prop){
+		var get = _get[prop] || _get['default'];
+		var val = get(el, prop);
+
+		return val;
+	};
+	var _default = {
+		'borderWidth': 1,
+		'paddingWidth': 2
+	};
+	var _tunningVal = 3;
+	var _get = {};
+	_get['width'] = _isBoxModel && 
+		function(el){
+			return (el.offsetWidth - 2 *_tunningVal) + 'px'; // 宽度比文本框小
+		} ||function(el){
+			var minuend = 0;
+			var props = ['border', 'padding'], dirs = ['Left', 'Right'];
+			for (var i = 0, prop; prop = props[i++];){
+				for (var j = 0, dir; dir = dirs[j++];){
+					minuend += (parseInt(_getComputedStyle(el, prop + dir)) || _default[prop + 'Width']);
+				}
+			}
+
+			var w = el.offsetWidth - minuend - 2 * _tunningVal;
+			return w + 'px';
 		};
+	_get['height'] = _get['lineHeight'] = _isBoxModel &&
+		function(el){
+			return el.offsetHeight + 'px';
+		} || function(el){
+			var minuend = 0;
+			var props = ['border', 'padding'], dirs = ['Top', 'Bottom'];
+			for (var i = 0, prop; prop = props[i++];){
+				for (var j = 0, dir; dir = dirs[j++];){
+					minuend += (parseInt(_getComputedStyle(el, prop + dir)) || _default[prop + 'Width']);
+				}
+			}
+
+			var h = el.offsetHeight - minuend;
+			return h + 'px';
+		};
+	_get['top'] = function(el){
+			var pos = _getComputedStyle(el, 'position');
+			if (!/^\s*relative|absolute\s*$/i.test(pos)){
+				el.style.position = 'relative';
+			}
+
+			return el.offsetTop + 'px';
+		};
+	_get['left'] = function(el){
+			var pos = _getComputedStyle(el, 'position');
+			if (!/^\s*relative|absolute\s*$/i.test(pos)){
+				el.style.position = 'relative';
+			}
+
+			return (el.offsetLeft + _tunningVal) + 'px';
+		};
+	_get['fontSize'] = function(el){
+			var val = _getComputedStyle(el, 'fontSize'), integer;
+			val = parseFloat(val), integer = parseInt(val);
+			val = val > integer ? integer + 1 : integer;
+
+			return val + 'px';
+		};
+
+	_get['default'] = function(el, prop){
+			return _getComputedStyle(el, prop);
+		};
+
+
+	var _tpl = '<span style="white-space:${whiteSpace};position:${position};overflow:${overflow};font-family:${fontFamily};color:${color};'+
+		'top:${top};left:${left};height:${height};width:${width};font-size:${fontSize};padding:${paddingTop} 0 ${paddingBottom} ${paddingLeft};line-height:${lineHeight};">' + 
+		'${innerHTML}</span>';
+	var _createHtml = function(el){
+		var kv = {
+				'whiteSpace': 'nowrap',
+				'position': 'absolute',
+				'overflow': 'hidden',
+				'fontFamily': 'arial,sans-serif',
+				'color': placeholder.color,
+				'innerHTML': el.getAttribute(placeholder.attr)
+			};
+		var props = ['top', 'left', 'height', 'width', 'fontSize', 'paddingLeft', 'paddingTop', 'paddingBottom', 'lineHeight'];
+		for (var i = 0, prop; prop = props[i++];){
+			kv[prop] = _getStyle(el, prop);
+		}
+		var html = _fmt(_tpl, kv);
+
+		return html;
 	};
 
-	var defaultBorderWidth = 1;
-	var color = '#888';
-	var _createPlaceholder = function(top, left, height, width, padding, fontSize, lineHeight, val){
-		var el = document.createElement('span');
-		el.style.border = 'solid 1px red';
-		el.style.whiteSpace = 'nowrap';
-		el.style.position = 'absolute';
-		el.style.overflow = 'hidden';
-		el.style.fontFamily = 'arial,sans-serif';
-
-		el.style.color = color;
-		el.style.top = top + 'px';
-		el.style.left = left + 'px';
-		el.style.height = (_isBoxModel ? height : (height - 6)) + 'px';
-		el.style.width = (_isBoxModel ? width : (width - 6)) + 'px';
-		el.style.fontSize = fontSize;
-		el.style.padding = "2px 2px";
-		el.style.lineHeight = lineHeight;
-		el.innerHTML = val;
-
-		return el;
-	};
-	var _attachPlaceholder = function(input, placeholder){
-		input.parentNode.appendChild(placeholder);
-	}	
-
-	var data_placeholder = 'data-placeholder';
 	var _proc = function(el){
-		var height = el.offsetHeight,
-			width = el.offsetWidth,
-			fontSize = _getComputedStyle(el, 'fontSize'),
-			padding = _getComputedStyle(el, 'padding'),
-			lineHeight = _getComputedStyle(el, 'lineHeight');
-		var topLeft = _getInputTopLeft(el);
-
-		var placeholder = _createPlaceholder(topLeft.top, 
-			topLeft.left, 
-			height, 
-			width,
-			padding,
-			fontSize, 
-			lineHeight,
-			el.getAttribute(data_placeholder));
-		_attachPlaceholder(el, placeholder);
+		var html = _createHtml(el);
+		var phel = _$(html);
+		el.parentNode.appendChild(phel);
 	};
 
 	exports.placeholder = function(){
   		var els = document.getElementsByTagName('input');
   		for (var i = 0, el; el = els[i++];){
-  			el.type === 'text' && _proc(el);
+  			el.type === 'text' && 
+				el.getAttribute(placeholder.attr) && 
+				_proc(el);
   		}
   	};
+	exports.placeholder.attr = 'data-placeholder';
+	exports.placeholder.color = '#888';
 }(window));
